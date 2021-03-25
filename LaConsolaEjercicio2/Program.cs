a1705e7cd1b17b33902e32cd71c7afc53c67167e
Pau Mezzadra <paumezzadra@hotmail.com>
2021-03-25 14:06:42 -0300

    Add project files.

---
+++ LaConsolaEjercicio2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaConsolaEjercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "";
+
+            do
+            {
+                Console.WriteLine("Ingrese cualquier texto:");
+                texto = Console.ReadLine().ToUpper();
+                if (texto == "")
+                {
+                    Console.WriteLine("Debe ingresar al menos una palabra");
+                }
+
+            } while (texto == "");
+
+            Console.WriteLine("Usted escribió: " + texto);
+            Console.WriteLine("Presione cualquier tecla para continuar");
+            Console.ReadKey();
+
+        }
+    }
+}
